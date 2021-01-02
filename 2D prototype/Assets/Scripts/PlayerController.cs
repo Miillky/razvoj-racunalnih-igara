da677bca5077a00f750559fe5424b7cf6aee9a1d
Vedran Milković <vedran.milkovic25@gmail.com>
2021-01-02 20:30:51 +0100

    Laboratorijska vježba 7

--- 2D prototype/Assets/Scripts/PlayerController.cs
+++ 2D prototype/Assets/Scripts/PlayerController.cs
@@ -9,16 +9,26 @@ public class PlayerController : MonoBehaviour {
     private Vector2 movement = new Vector2();
     private Rigidbody2D rigidBody;
 
-    [SerializeField]
-    private GameObject prefab;
+    private Animator animator;
+    private string animationState = "AnimationState";
+
+    enum AnimStates {
+        idle = 0,
+        walk_down = 1,
+        walk_right = 2,
+        walk_up = 3,
+        walk_left = 4,
+    }
 
     // Start is called before the first frame update
     void Start(){
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update(){
         MovementHandler();
+        AnimationHandler();
     }
     private void MovementHandler(){
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -26,5 +36,19 @@ public class PlayerController : MonoBehaviour {
         Debug.Log("X: " + movement.x + " " + "Y: " + movement.y);
         movement.Normalize();
         rigidBody.velocity = movement * movementSpeed;
+    }
+
+    private void AnimationHandler(){
+        if(movement.x > 0){
+            animator.SetInteger(animationState, (int)AnimStates.walk_right);
+        } else if(movement.x < 0){
+            animator.SetInteger(animationState, (int)AnimStates.walk_left);
+        } else if(movement.y > 0){
+            animator.SetInteger(animationState, (int)AnimStates.walk_up);
+        } else if(movement.y < 0){
+            animator.SetInteger(animationState, (int)AnimStates.walk_down);
+        } else {
+            animator.SetInteger(animationState, (int)AnimStates.idle);
+        }
     }
 }
